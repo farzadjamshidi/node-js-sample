03d5455abb45382d6d89dfead78161ccc37f7ceb
farzadjamshidi <f_jblack@yahoo.com>
2025-03-06 16:40:14 +0000

    Add swagger annotation to the routes

--- shop/src/routes/routes.js
+++ shop/src/routes/routes.js
@@ -4,7 +4,39 @@ import { getProducts, getProductById } from "../controllers/product.controller.j
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/shop/products:
+ *   get:
+ *     summary: Get products
+ *     description: Get all products
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved products.
+ *       404:
+ *         description: User not found.
+ */
 router.get("/products", getProducts);
+
+/**
+ * @swagger
+ * /api/shop/products/{id}:
+ *   get:
+ *     summary: Get product by ID
+ *     description: Retrieve a single product by their ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the product to retrieve.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved product.
+ *       404:
+ *         description: User not found.
+ */
 router.get("/products/:id", getProductById);
 
 export default router;
--- social-network/src/routes/routes.js
+++ social-network/src/routes/routes.js
@@ -4,8 +4,60 @@ import { getPosts, getPostById, getPostsByUserId } from "../controllers/post.con
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/social-network/posts:
+ *   get:
+ *     summary: Get posts
+ *     description: Get all posts
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved posts.
+ *       404:
+ *         description: User not found.
+ */
 router.get("/posts", getPosts);
+
+/**
+ * @swagger
+ * /api/social-network/posts/{id}:
+ *   get:
+ *     summary: Get post by ID
+ *     description: Retrieve a single post by their ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the post to retrieve.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved post.
+ *       404:
+ *         description: User not found.
+ */
 router.get("/posts/:id", getPostById);
+
+/**
+ * @swagger
+ * /api/social-network/posts/user/{id}:
+ *   get:
+ *     summary: Get post by user ID
+ *     description: Retrieve all posts of a specific user by user ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the user to retrieve.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved all posts of a specific user.
+ *       404:
+ *         description: User not found.
+ */
 router.get("/posts/user/:id", getPostsByUserId);
 
 export default router;
